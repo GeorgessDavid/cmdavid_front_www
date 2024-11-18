344b95ca0262d73b0e7b70726ac6e8087edf0b79
Georges David <georgessdavid@gmail.com>
2024-11-18 20:53:23 -0300

    added medical procedures.

---
+++ src/hooks/useMedicalProcedures.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+export const useMedicalProcedures = () => {
+    const [medicalProcedures, setMedicalProcedures] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        setLoading(10);
+        const requestMedicalProcedures = async () => {
+            setProgress(20);
+            try {
+                const response = await fetch('https://api-cmdavid.vercel.app/medicalProcedures')
+                setProgress(70);
+                const data = await response.json();
+                setProgress(100);
+                setMedicalProcedures(data)
+                setLoading(false)
+            } catch (error) {
+                setError(error)
+                setLoading(false)
+            }
+        }
+
+        requestMedicalProcedures()
+    }, [])
+    return { medicalProcedures, loading, error, progress };
+}
--- src/pages/Estudios/Estudios.jsx
+++ src/pages/Estudios/Estudios.jsx
@@ -1,7 +1,31 @@
+import { useMedicalProcedures } from '../../hooks/useMedicalProcedures';
+import { ItemBox } from '../../components/ItemBox/ItemBox';
+import { LoadingPage } from '../LoadingPage/LoadingPage';
+import './Estudios.css'
+
 export const Estudios = () => {
+    const { medicalProcedures, loading, progress } = useMedicalProcedures();
+
+    if (loading) {
+        return (
+            <>
+                <LoadingPage progress={progress} />
+            </>
+        )
+    }
     return (
-        <div>
+        <section className="specialties_container">
+
             <h1>Estudios</h1>
-        </div>
+            <div>
+                {
+                    medicalProcedures.map((procedure, index) =>
+                        <ItemBox id={procedure.id} class_name="procedure_itemBox" title={procedure.practicaMedica} key={index} />
+                    )
+                }
+            </div>
+
+        </section>
+
     )
 }
