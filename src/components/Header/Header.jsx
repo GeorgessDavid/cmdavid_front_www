e673e17b7844bfafaedccd3e9351e1a7617d636d
Georges David <georgessdavid@gmail.com>
2026-01-02 18:35:29 -0300

    Modal agregado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,27 @@
 import { FirstHeader } from './FirstHeader/FirstHeader'
 import { SecondHeader } from './SecondHeader/SecondHeader'
 import { HamburguerMenu } from './HamburguerMenu/HamburguerMenu'
+import { Alert } from '@mui/material';
 import { useMediaQuery } from '../../hooks/useMediaQueries'
+import { useState, useEffect } from 'react';
 import './Header.css'
 
 export const Header = () => {
     const mobile = useMediaQuery('(min-width: 850px)');
-
+    const [alertVisible, setAlertVisible] = useState(sessionStorage.getItem('homeModalClosed') ? true : false);
+    
+    useEffect(() => {
+        const handleStorageChange = () => {
+            setAlertVisible(sessionStorage.getItem('homeModalClosed') ? true : false);
+        };
+        
+        window.addEventListener('homeModalClosed', handleStorageChange);
+        
+        return () => {
+            window.removeEventListener('homeModalClosed', handleStorageChange);
+        };
+    }, []);
+    
     return (
         <header>
             {!mobile ?
@@ -17,6 +32,11 @@ export const Header = () => {
                     <FirstHeader />
                     <SecondHeader />
                 </>
+            }
+            {alertVisible &&
+                <Alert severity="warning" style={{ position: 'fixed', top: '14vh', left: '15vw', zIndex: 1000, maxWidth: '70%', margin: '0 auto' }} >
+                    🏥 <strong>Importante:</strong> A partir del <strong>Lunes 5 de Enero de 2026</strong>, Consultorios Médicos David se traslada a <strong>Padre Silberman 1346, Santos Lugares</strong>. ¡Los esperamos en nuestra nueva <a href="https://maps.app.goo.gl/LLXuZfKs39o8zbZJ7" target="_blank" rel="noopener noreferrer">ubicación</a>!
+                </Alert>
             }
         </header>
     )
